a8d89b55d3b07f7ff7fbc8ad9bd41272e51805d7
Ayati <ayatiogo@gmail.com>
2025-08-26 09:13:13 +0100

    staticfiles collected

--- django_api/settings.py
+++ django_api/settings.py
@@ -125,7 +125,7 @@ MEDIA_URL = 'img/'
 MEDIA_ROOT = BASE_DIR/'media'
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILE_DIRS = STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
